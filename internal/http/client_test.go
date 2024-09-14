279e5ae1bad497b3167796d35c366882bca7e461
macie <macie@users.noreply.github.com>
2024-09-14 21:21:04 +0200

    ci: Fix errcheck violation
    See: <https://github.com/kisielk/errcheck>.

--- internal/http/client_test.go
+++ internal/http/client_test.go
@@ -19,7 +19,9 @@ func ExampleGet() {
 	c := Client{buildOS: "anyOS", AppVersion: "0.0.0-local"}
 	URL := "https://httpbin.org/get"
 	raw := ensure.MustReturn(c.Get(context.TODO(), URL))
-	defer raw.Body.Close()
+	defer func() {
+		_ = raw.Body.Close() // ignore closing errors during testing
+	}()
 
 	ensure.Must(json.NewDecoder(raw.Body).Decode(&response))
 
